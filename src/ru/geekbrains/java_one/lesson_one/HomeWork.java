2fc2ea558535b3a0adbdd918ff8935183501ccaf
senyabird <senyabird@yandex.ru>
2020-04-06 14:08:29 +0300

    Изменил код в HomeWork.java

--- src/ru/geekbrains/java_one/lesson_one/HomeWork.java
+++ src/ru/geekbrains/java_one/lesson_one/HomeWork.java
@@ -7,57 +7,55 @@ public class HomeWork {
         System.out.println("Результат выполнения метода doMath: " + res); // Проверка метода doMath
 
         // Вызов метода checkSum с проверкой результатов для нескольких случаев...
-        System.out.println("Результат выполнения метода checkSum(12, 5): " + checkSum(12, 5));
-        System.out.println("Результат выполнения метода checkSum(3, 5): " + checkSum(3, 5));
-        System.out.println("Результат выполнения метода checkSum(-12, 35): " + checkSum(-12, 35));
-        System.out.println("Результат выполнения метода checkSum(-12, -5): " + checkSum(-12, -5));
+        boolean rangeOfSum = checkSum(12, 5);
+        System.out.println("Рузультат выполнения метода checkSum(12, 5): " + rangeOfSum);
+        rangeOfSum = checkSum(3, 5);
+        System.out.println("Рузультат выполнения метода checkSum(3, 5): " + rangeOfSum);
+        rangeOfSum = checkSum(-12, 25);
+        System.out.println("Рузультат выполнения метода checkSum(-12, 25): " + rangeOfSum);
+        rangeOfSum = checkSum(-12, 0);
+        System.out.println("Рузультат выполнения метода checkSum(-12, 0): " + rangeOfSum);
 
-        // Вызов метода signOfNumber с проверкой результатов для нескольких случаев...
-        signOfNumber(5);
-        signOfNumber(0);
-        signOfNumber(-3);
+        // Вызов метода isPositive с проверкой результатов для нескольких случаев...
+        System.out.println("Число 5 положительно: " + isPositive(5));
+        System.out.println("Число 0 положительно: " + isPositive(0));
+        System.out.println("Число 5 положительно: " + isPositive(-3));
 
-        // Вызов метода greeting...
-        greeting("Арсений");
-        greeting("Иван");
+        // Вызов метода greetings...
+        greetings("Арсений");
+        greetings("Иван");
 
         // Вызов метода isLeapYear и проверка результатов для нескольких вариантов
-        isLeapYear(1300);
-        isLeapYear(1200);
-        isLeapYear(2048);
-        isLeapYear(2047);
-        isLeapYear(2100);
-        isLeapYear(1500);
-        isLeapYear(1400);
+        System.out.println("1300 год является високосным: " + isLeapYear(1300));
+        System.out.println("2048 год является високосным: " + isLeapYear(2048));
+        System.out.println("2047 год является високосным: " + isLeapYear(2047));
+        System.out.println("2100 год является високосным: " + isLeapYear(2100));
+        System.out.println("1500 год является високосным: " + isLeapYear(1500));
+        System.out.println("1400 год является високосным: " + isLeapYear(1400));
+        System.out.println("2000 год является високосным: " + isLeapYear(2000));
     }
 
     private static double doMath(int a, int b, int c, int d) {
-        double result = a * (b + (c / d));
+        double result = a * (b + (1.0 * c / d));
         return result;
     }
 
     private static boolean checkSum(int x, int y) {
-        if ((x + y) >= 10 && (x + y) <= 20)
-            return true;
-        else
-            return false;
+        boolean isSumCorrect = ((x + y >= 10) && (x + y <= 20));
+        return isSumCorrect;
     }
 
-    private static void signOfNumber(int a) {
-        if(a >= 0)
-            System.out.println("Число " + a + " положительно!");
-        else
-            System.out.println("Число " + a + " отрицательно!");
+    private static boolean isPositive(int a) {
+        boolean bool = a >= 0;
+        return bool;
     }
 
-    private static void greeting(String name) {
+    private static void greetings(String name) {
         System.out.println("Привет, " + name);
     }
 
-    private static void isLeapYear(int year) {
-        if ((year % 4 == 0) && !(year % 400 == 100) && !(year % 400 == 200) && !(year % 400 == 300))
-            System.out.println(year + " - високосный год!");
-        else
-            System.out.println(year + " не является високосным годом!");
+    private static boolean isLeapYear(int year) {
+        boolean isLeap = ((year % 4 == 0) && !(year % 400 == 100) && !(year % 400 == 200) && !(year % 400 == 300));
+        return isLeap;
     }
 }
