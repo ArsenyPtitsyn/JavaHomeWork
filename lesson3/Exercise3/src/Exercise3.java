81b4575b26585e26ad4ae852fb96bc4ee68815c1
senyabird <senyabird@yandex.ru>
2020-04-12 17:16:20 +0300

    Создан новый проект Exercise3

---
+++ lesson3/Exercise3/src/Exercise3.java
@@ -0,0 +1,149 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercise3 {
+
+    private static int fieldSizeY;
+    private static int fieldSizeX;
+    private static char[][] field;
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final Random RANDOM = new Random();
+
+    private static final char HUMAN_DOT = 'X';
+    private static final char AI_DOT = 'O';
+    private static final char EMPTY_DOT = '.';
+
+    // init field
+    private static void initMap() {
+        fieldSizeY = 3;
+        fieldSizeX = 3;
+        field = new char[fieldSizeY][fieldSizeX];
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                field[y][x] = EMPTY_DOT;
+            }
+        }
+    }
+
+    // print field
+    private static void printMap() {
+        System.out.println("-------");
+        for (int y = 0; y < fieldSizeY; y++) {
+            System.out.print("|");
+            for (int x = 0; x < fieldSizeX; x++) {
+                System.out.print(field[y][x] + "|");
+            }
+            System.out.println();
+        }
+    }
+
+    // human turn
+    private static void humanTurn() {
+        int x;
+        int y;
+        do {
+            System.out.printf("Введите координаты хода X и Y (от 1 до %d) через пробел: ", fieldSizeX);
+            x = SCANNER.nextInt() - 1;
+            y = SCANNER.nextInt() - 1;
+        } while (!(isEmptyCell(y, x) && isValidCell(y, x)));
+        field[y][x] = HUMAN_DOT;
+    }
+
+    // is cell empty
+    private static boolean isEmptyCell(int y, int x) {
+        return field[y][x] == EMPTY_DOT;
+    }
+
+    // is cell valid
+    private static boolean isValidCell(int y, int x) {
+        return x >= 0 && x < fieldSizeX && y >= 0 && y < fieldSizeY;
+    }
+
+    // ai turn
+    private static void aiTurn() {
+        int x;
+        int y;
+        do {
+            x = RANDOM.nextInt(fieldSizeX);
+            y = RANDOM.nextInt(fieldSizeY);
+        } while (!isEmptyCell(y, x));
+        field[y][x] = AI_DOT;
+    }
+
+    // check win
+    private static boolean checkWin(char c) {
+
+        for(int i = 0; i < fieldSizeY; i++) {
+            boolean res = field[i][0] == c;
+            for(int j = 1; j < fieldSizeX && res; j++) {
+                res = field[i][j] == c;
+            }
+            if(res) return true;
+        }
+
+        for(int i = 0; i < fieldSizeX; i++) {
+            boolean res = field[0][i] == c;
+            for(int j = 1; j < fieldSizeY && res; j++) {
+                res = field[j][i] == c;
+            }
+            if(res) return true;
+        }
+
+        boolean res = field[0][0] == c;
+        for(int i = 1; i < fieldSizeX && i < fieldSizeY && res; i++) {
+            res = field[i][i] == c;
+        }
+        if(res) return true;
+
+        boolean res1 = field[0][2] == c;
+        for(int i = 1; i < fieldSizeX && i < fieldSizeY && res1; i++) {
+            res1 = field[i][fieldSizeX - 1 - i] == c;
+        }
+        if(res1) return true;
+
+        return false;
+    }
+
+    // check draw
+    private static boolean isMapFull() {
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                if (isEmptyCell(y, x)) return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        while (true) {
+            initMap();
+            printMap();
+            while (true) {
+                humanTurn();
+                printMap();
+                if (gameChecks(HUMAN_DOT, "Human win!")) break;
+                aiTurn();
+                printMap();
+                if (gameChecks(AI_DOT, "AI win!")) break;
+            }
+            System.out.println("Play again?");
+            if (!SCANNER.next().equals("y"))
+                break;
+        }
+        SCANNER.close();
+
+    }
+
+    private static boolean gameChecks(char aiDot, String s) {
+        if (checkWin(aiDot)) {
+            System.out.println(s);
+            return true;
+        }
+        if (isMapFull()) {
+            System.out.println("draw!");
+            return true;
+        }
+        return false;
+    }
+}
