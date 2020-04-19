5059642c0f3d92fe1ca0819f1cbd4b04fb5d5db4
senyabird <senyabird@yandex.ru>
2020-04-19 13:51:23 +0300

    Вариант преподавателя 4 домашки

--- src/lesson4/Employee.java
+++ src/lesson4/Employee.java
@@ -1,60 +1,82 @@
 package lesson4;
 // 1. Create class "Employee" with the fields: full name, salary and age.
 public class Employee {
+    private static final int CURRENT_YEAR = 2018;
 
-    private String fullName;
-    private int birthDay;
-    private int salary;
-    private int id;
+    private String surname;
+    private String secondName;
+    private String name;
+    private String position;
+    private String phone;
+    private float salary;
+    boolean isSalaryChanged;
+    private int birthYear;
+    private static int id = 0;
+    public int uid;
 
-    private int year = 2020;
-    private static int index = 0;
-    // 2. The class constructor must fill this fields when creating the object
-    // 8. Think through the constructor so that each employee is assigned a
-    // personal unique identification sequence number when creating it
-    public Employee(String fullName, int salary, int birthDay) {
-        this.id = index++;
-        this.fullName = fullName;
+    // 2.	Конструктор класса должен заполнять эти поля при создании объекта;
+    Employee (String name,
+              String secondName,
+              String surname,
+              String phone,
+              String position,
+              float salary,
+              int birthYear) {
+        this.birthYear = birthYear;
+        this.name = name;
+        this.surname = surname;
+        this.secondName = secondName;
+        this.position = position;
+        this.phone = phone;
         this.salary = salary;
-        this.birthDay = birthDay;
-    }
-    // 3. Inside the employee class, write methods that return the value of each field
-    public String getFullName() {
-        return fullName;
+        this.uid = ++id;
     }
 
-    public int getAge() {
-        return year - birthDay;
+    // 3.	Внутри класса «Сотрудник» написать методы, которые возвращают
+    // значение каждого поля;
+    int getAge() {
+        return CURRENT_YEAR - birthYear;
     }
 
-    public int getSalary() {
-        return salary;
+    int getSalary() {
+        return (int)salary;
     }
 
-    public int getId() {
-        return id;
+    void setSalary(float difference) {
+        this.salary = this.salary + difference;
     }
-    // 6. Create method that increases the salary of all employees over 45 years of age by 5000.
-    public static void increaseSalary(Employee[] e) {
-        for(int i = 0; i < e.length; i++) {
-            if(e[i].getAge() > 45)
-                e[i].salary += 5000;
-        }
+
+    void setSalary(int salary) {
+        this.salary = salary;
     }
-    // 7a. To calculate the average age.
-    public static float averageOfTheAges(Employee[] e) {
-        int sumOfAges = 0;
-        for(int i = 0; i < e.length; i++) {
-            sumOfAges += e[i].getAge();
-        }
-        return sumOfAges / (float)e.length;
+
+    String getSecondName() {
+        return secondName;
     }
-    // 7b. To calculate the average salary.
-    public static float averageOfTheSalaries(Employee[] e) {
-        int sumOfSalaries = 0;
-        for(int i = 0; i < e.length; i++) {
-            sumOfSalaries += e[i].salary;
-        }
-        return sumOfSalaries / (float)e.length;
+
+    String getName() {
+        return name;
+    }
+
+    String getSurname() {
+        return surname;
+    }
+
+    String getPosition() {
+        return position;
+    }
+
+    String getPhone() {
+        return phone;
+    }
+
+    String getFullInfo() {
+        return  this.name + " " +
+                this.secondName + " " +
+                this.surname + ", " +
+                this.getAge() + " years old, " +
+                this.position + ". Phone number: " +
+                this.phone + ". Salary is " +
+                this.getSalary() + " RUR.";
     }
 }
--- src/lesson4/HomeWork4.java
+++ src/lesson4/HomeWork4.java
@@ -3,65 +3,84 @@ package lesson4;
 import java.util.Arrays;
 
 public class HomeWork4 {
+
+    // 6.	* Создать метод, повышающий зарплату всем сотрудникам старше 45 лет на 5000.
+    // Метод должен принимать в качестве параметра массив сотрудников.
+    // Вывести в main() возраст и зарплату сотрудника до и после вызова метода;
+    private static void increaser(Employee[] emp, byte age, float increment){
+        for (int i = 0; i < emp.length; i++) {
+            if (emp[i].getAge() > age) {
+                emp[i].setSalary(increment);
+                emp[i].isSalaryChanged = true;
+            }
+        }
+    }
+
+    // 7.	** Написать методы (принимающие на вход массив сотрудников),
+    // вычисляющие средний возраст и среднюю зарплату сотрудников,
+    // вывести результаты работы в консоль.
+    private static float averageSalary(Employee[] emp){
+        float result = 0;
+        for (int i = 0; i < emp.length; i++)
+            result += emp[i].getSalary();
+
+        return result / emp.length;
+    }
+
+    private static float averageAge(Employee[] emp){
+        float result = 0;
+        for (int i = 0; i < emp.length; i++)
+            result += emp[i].getAge();
+
+        return result / emp.length;
+    }
+
     public static void main(String[] args) {
-        double averageAge, averageSalary;
-        // 5. Create array of 5 employees
-        Employee[] employees = new Employee[5];
-        // Initialization
-        employees[0] = new Employee("Pupkin Vasily Ivanovich", 32154, 1995);
-        employees[1] = new Employee("Ivanov Ivan Ivanovich", 37162, 1975);
-        employees[2] = new Employee("Popov Igor Konstantinovich", 57123, 1970);
-        employees[3] = new Employee("Drozdov Nikolay Egorovich", 123427, 1968);
-        employees[4] = new Employee("Terkin Vasily Nikolaevich", 27953, 1980);
-        // 4. Use the methods to get the full name and age for all employees.
-        printEmployeeInfo(employees[0]);
-        printEmployeeInfo(employees[1]);
-        printEmployeeInfo(employees[2]);
-        printEmployeeInfo(employees[3]);
-        printEmployeeInfo(employees[4]);
+        // 1.	Создать класс "Сотрудник" с полями: ФИО, должность, телефон,
+        // зарплата, возраст;
+        Employee e = new Employee("Arseny", "Grigorievich",
+                "Ptitsyn", "8(495)000-11-22",
+                "physicist", 15000, 1982);
 
-        System.out.println();
+        // 4.	Вывести при помощи методов из пункта 3 ФИО и должность.
+        System.out.println("Please welcome our " + e.getPosition() + ", " +
+                e.getName() + " " + e.getSecondName() + " " + e.getSurname() + ".");
 
-        // Output information about employees over 40 years of age
-        employeesOverFortyYearsInfo(employees);
+        // 5.	Создать массив из 5 сотрудников. С помощью цикла вывести
+        // информацию только о сотрудниках старше 40 лет;
 
-        System.out.println();
+        Employee[] employees = {
+                e,
+                new Employee("Andrey", "Viktorovich",
+                        "Bezrukov", "8(495)111-22-33",
+                        "fitter", 52000, 1973),
+                new Employee("Evgeniy", "Viktorovich",
+                        "Del'finov", "8(495)222-33-44",
+                        "project manager", 40000, 1963),
+                new Employee("Natalia", "Pavlovna",
+                        "Keks", "8(495)333-44-55",
+                        "senior developer", 90000, 1990),
+                new Employee("Tatiana", "Sergeevna",
+                        "Krasotkina", "8(495)444-55-66",
+                        "accountant", 50000, 1983)
+        };
 
-        // Salary increase for employees over 45 years of age
-        System.out.println("Salary before: " + employees[2].getSalary() + " and " +
-                employees[3].getSalary());
-        Employee.increaseSalary(employees);
-        System.out.println("Salary after: " + employees[2].getSalary() + " and " +
-                employees[3].getSalary());
+        for (int i = 0; i < employees.length; i++)
+            if (employees[i].getAge() > 40)
+                System.out.println(employees[i].getFullInfo());
 
-        System.out.println();
+        System.out.println("--------------------------------------------------------");
+        for (int i = 0; i < employees.length; i++)
+            System.out.println(employees[i].getFullInfo());
 
-        // Averages of ages and salaries of all employees
-        System.out.println("Average ages and salaries of all emloyees are: " +
-                Employee.averageOfTheAges(employees) + " and " +
-                Employee.averageOfTheSalaries(employees));
+        increaser(employees, (byte)45, 5000.0f);
 
-        System.out.println();
-
-        // Output information about identification numbers of employees.
-        printId(employees);
-    }
-
-    private static void employeesOverFortyYearsInfo(Employee[] e) {
-        for(int i = 0; i < e.length; i++) {
-            if(e[i].getAge() > 40)
-                printEmployeeInfo(e[i]);
-        }
-    }
-
-    private static void printEmployeeInfo(Employee e) {
-        System.out.println(e.getFullName() + ". Age: " +
-                e.getAge());
-    }
-
-    private static void printId(Employee[] e) {
-        for(int i = 0; i < e.length; i++) {
-            System.out.println(e[i].getFullName() + " id: " + e[i].getId());
-        }
+        System.out.println("--------------------------------------------------------");
+        for (int i = 0; i < employees.length; i++)
+            if (employees[i].isSalaryChanged)
+                System.out.println(employees[i].getFullInfo());
+        System.out.println("--------------------------------------------------------");
+        System.out.println("Avg. age: " + averageAge(employees) + " years");
+        System.out.println("Avg. salary: " + averageSalary(employees) + " RUR");
     }
 }
